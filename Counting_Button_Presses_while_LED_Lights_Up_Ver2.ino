c98caf0e7ab62c29e1b641a6ed5f839d8e790dc9
Haow1 <aidenwen94@gmail.com>
2017-07-08 23:16:14 +0200

    Add files via upload

---
+++ Counting_Button_Presses_while_LED_Lights_Up_Ver2.ino
@@ -0,0 +1,32 @@
+int ledPin = 6;
+const int buttonPin = 7;
+
+static int count = 0;
+int buttonState = 0;
+int preButtonState = 0;
+
+void setup() {
+Serial.begin(9600);
+pinMode(ledPin, OUTPUT);
+pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+buttonState = digitalRead(buttonPin);
+
+digitalWrite(ledPin, HIGH);
+
+unsigned long time = millis ();
+
+while( millis()-time < 300) {
+
+  buttonState = digitalRead(buttonPin);
+  if (buttonState != preButtonState ){
+    if (buttonState == LOW){
+      count = count + 1;
+    }
+  }
+} digitalWrite(ledPin, LOW);
+  delay(10);
+
+}
